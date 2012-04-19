082491fad1afbe9939e67aef9b57b491868bd666
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2012-04-19 16:09:21 -0600

    add an initial file

--- modules/httpd/manifests/init.pp
+++ modules/httpd/manifests/init.pp
@@ -1,14 +1,18 @@
 # Basic Puppet Apache manifest
 
 class httpd {
-  package { "httpd":
+  package { 'httpd':
     ensure => present,
   }
 
-  service { "httpd":
-    ensure => running,
-    require => Package["httpd"],
+  service { 'httpd':
+    ensure  => running,
+    require => File['index.html'],
   }
+
+  file { 'index.html':
+      path   => '/var/www/index.html',
+      ensure => file,
+      require => Package['httpd'],
+      source  => 'puppet:///modules/httpd/index.html',
 }
-
-
