5f7b1babc6b4d21be2f41de59ad81682fefcd614
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2012-04-27 16:33:05 -0600

    Rearrange the puppet config files.

---
+++ manifests/nodes.pp
@@ -0,0 +1,4 @@
+node default {
+  include httpd
+  include motd
+}
---
+++ modules/motd/manifests/init.pp
@@ -0,0 +1,15 @@
+# An example Puppet manifest to provision the message of the day:
+#
+
+class motd {
+  group { "puppet":
+    ensure => "present",
+  }
+
+  File { owner => 0, group => 0, mode => 0644 }
+
+  file { '/etc/motd':
+    content => "Welcome to your Vagrant-built virtual machine!
+  	      Managed by Puppet.\n"
+  }
+}
